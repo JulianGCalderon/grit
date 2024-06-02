c54dd2487788a6a3d9e10359c52f86e5555d636c
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-06-02 13:14:34 -0300

    Add basic init implementation

--- src/command.rs
+++ src/command.rs
@@ -1,4 +1,8 @@
-use std::{fs::create_dir_all, io};
+use std::{
+    fs::{create_dir_all, write},
+    io,
+    path::Path,
+};
 
 use thiserror::Error;
 
@@ -13,7 +17,42 @@ pub enum GitError {
 pub type GitResult<T> = Result<T, GitError>;
 
 pub fn init() -> GitResult<()> {
-    create_dir_all(GIT_DIR)?;
+    let git_dir = Path::new(GIT_DIR);
+    create_dir_all(git_dir)?;
+
+    let head = git_dir.join("HEAD");
+    if !head.exists() {
+        write(head, "ref: refs/heads/master\n")?;
+    }
+
+    let config = git_dir.join("config");
+    if !config.exists() {
+        let contents = "\
+            [core]\n\
+            \trepositoryformatversion = 0\n\
+            \tfilemode = true\n\
+            \tbare = false\n\
+            \tlogallrefupdates\n";
+
+        write(config, contents)?;
+    }
+
+    let branches = git_dir.join("branches");
+    create_dir_all(branches)?;
+    let hooks = git_dir.join("hooks");
+    create_dir_all(hooks)?;
+    let info = git_dir.join("info");
+    create_dir_all(info)?;
+    let objects = git_dir.join("objects");
+    let objects_info = objects.join("objects_info");
+    let objects_pack = objects.join("objects_pack");
+    create_dir_all(objects_info)?;
+    create_dir_all(objects_pack)?;
+    let refs = git_dir.join("refs");
+    let refs_heads = refs.join("heads");
+    let refs_tags = refs.join("tags");
+    create_dir_all(refs_heads)?;
+    create_dir_all(refs_tags)?;
 
     Ok(())
 }
