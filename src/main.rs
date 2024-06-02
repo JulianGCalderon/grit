3314de326d9de704954718b94247d06ff93bb604
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-06-02 12:25:06 -0300

    Add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,23 @@
-fn main() {}
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Init,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Command::Init => {
+            println!("init");
+        }
+    }
+}
