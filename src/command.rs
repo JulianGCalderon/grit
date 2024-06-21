3f8e6c4ad8d0117de1d7f93379a5897434288b30
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-06-21 07:25:54 -0900

    Use GRIT_DIR env

--- src/command.rs
+++ src/command.rs
@@ -1,12 +1,14 @@
 use std::{
+    env,
     fs::{create_dir_all, remove_dir_all, write},
     io,
-    path::Path,
+    path::PathBuf,
 };
 
 use thiserror::Error;
 
 const GIT_DIR: &str = ".grit";
+const GIT_DIR_ENV: &str = "GRIT_DIR";
 
 #[derive(Error, Debug)]
 pub enum GitError {
@@ -17,10 +19,14 @@ pub enum GitError {
 pub type GitResult<T> = Result<T, GitError>;
 
 pub fn init() -> GitResult<()> {
-    let git_dir = Path::new(GIT_DIR);
+    let git_dir = {
+        let git_dir = env::var(GIT_DIR_ENV);
+        let git_dir = git_dir.as_deref().unwrap_or(GIT_DIR);
+        PathBuf::from(git_dir)
+    };
 
-    remove_dir_all(git_dir)?;
-    create_dir_all(git_dir)?;
+    let _ = remove_dir_all(&git_dir);
+    create_dir_all(&git_dir)?;
 
     let head = git_dir.join("HEAD");
     if !head.exists() {
@@ -40,16 +46,18 @@ pub fn init() -> GitResult<()> {
     }
 
     let branches = git_dir.join("branches");
-    create_dir_all(branches)?;
     let hooks = git_dir.join("hooks");
-    create_dir_all(hooks)?;
     let info = git_dir.join("info");
+    create_dir_all(branches)?;
+    create_dir_all(hooks)?;
     create_dir_all(info)?;
+
     let objects = git_dir.join("objects");
     let objects_info = objects.join("objects_info");
     let objects_pack = objects.join("objects_pack");
     create_dir_all(objects_info)?;
     create_dir_all(objects_pack)?;
+
     let refs = git_dir.join("refs");
     let refs_heads = refs.join("heads");
     let refs_tags = refs.join("tags");
