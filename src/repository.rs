d72afe1f868a00cbb59fb8a67b24aaa81ce9f48a
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-07-07 12:54:32 -0300

    Add get_object_path function

--- src/repository.rs
+++ src/repository.rs
@@ -26,6 +26,11 @@ pub fn get_git_dir() -> PathBuf {
     PathBuf::from(git_dir)
 }
 
+pub fn get_object_path(git_dir: &Path, oid: &Oid) -> PathBuf {
+    let oid_string = oid.to_string();
+    git_dir.join(format!("objects/{}/{}", &oid_string[..2], &oid_string[2..]))
+}
+
 pub fn blob(path: &Path) -> GitResult<Oid> {
     let git_dir = get_git_dir();
 
@@ -33,20 +38,14 @@ pub fn blob(path: &Path) -> GitResult<Oid> {
     let length = file.metadata()?.len() as usize;
 
     let blob_id = Blob::hash(&mut file, length)?;
-    let blob_id_string = blob_id.to_string();
 
-    let blob_path = git_dir.join(format!(
-        "objects/{}/{}",
-        &blob_id_string[..2],
-        &blob_id_string[2..]
-    ));
+    let blob_path = get_object_path(&git_dir, &blob_id);
     if let Some(base) = blob_path.parent() {
         create_dir_all(base)?;
     };
     let blob_file = File::create(blob_path)?;
 
     file.seek(io::SeekFrom::Start(0))?;
-
     Blob::serialize(file, blob_file, length)?;
 
     Ok(blob_id)
