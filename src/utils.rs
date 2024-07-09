21c5458818b535bd0c53f80c2b8795b9b2cc0fe9
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-07-08 23:47:23 -0300

    Remove comment

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,5 @@
 use std::ops::{BitAnd, Shr};
 
-// only receiving mask is probably simpler to read
 pub fn extract_bits<N>(original: N, mask: N, location: u8) -> N
 where
     N: Shr<u8, Output = N>,
