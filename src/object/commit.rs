1384570d6dc8ccfe765ad492863f7b1ebbc9bcbf
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-07-09 12:38:53 -0300

    Fix bug: hash with zlib
    Hash for Commit was being to be calculated from the compressed data, it
    should be calculated from plain data

--- src/object/commit.rs
+++ src/object/commit.rs
@@ -49,27 +49,24 @@ impl Commit {
 
     pub fn hash(&self) -> Oid {
         let mut hasher = Sha1::new();
-        let header = self.header();
 
-        hasher.update(&header);
-        self.serialize(&mut hasher)
+        self.serialize_without_compression(&mut hasher)
             .expect("writing to hasher cannot fail");
 
         let raw_id = hasher.finalize().into();
 
         Oid::from_raw_bytes(raw_id)
     }
-    pub fn serialize<W: Write>(&self, writer: W) -> GitResult<()> {
-        let mut encoder = ZlibWriteEncoder::new(writer, Compression::default());
 
-        encoder.write_all(&self.header())?;
+    pub fn serialize_without_compression<W: Write>(&self, mut writer: W) -> GitResult<()> {
+        writer.write_all(&self.header())?;
 
         let tree_line = format!("tree {}\n", self.tree_id);
-        encoder.write_all(tree_line.as_bytes())?;
+        writer.write_all(tree_line.as_bytes())?;
 
         for parent in &self.parents {
             let parent_line = format!("parent {}\n", parent);
-            encoder.write_all(parent_line.as_bytes())?;
+            writer.write_all(parent_line.as_bytes())?;
         }
 
         let timestamp = chrono::Local::now().format("%s %z");
@@ -78,15 +75,23 @@ impl Commit {
             "author {} <{}> {}\n",
             self.author, self.author_email, timestamp
         );
-        encoder.write_all(author_line.as_bytes())?;
+        writer.write_all(author_line.as_bytes())?;
 
         let commiter_line = format!(
             "committer {} <{}> {}\n\n",
             self.commiter, self.commiter_email, timestamp
         );
-        encoder.write_all(commiter_line.as_bytes())?;
+        writer.write_all(commiter_line.as_bytes())?;
 
-        encoder.write_all(self.message.as_bytes())?;
+        writer.write_all(self.message.as_bytes())?;
+
+        Ok(())
+    }
+
+    pub fn serialize<W: Write>(&self, writer: W) -> GitResult<()> {
+        let encoder = ZlibWriteEncoder::new(writer, Compression::default());
+
+        self.serialize_without_compression(encoder)?;
 
         Ok(())
     }
